53b76c2b4359c6a9109300d8ba1f9f615a3bf24d
Neko Ayaka <neko@ayaka.moe>
2023-09-22 13:59:48 +0800

    release(element-transform): release v1.0.2

--- packages/markdown-it-element-transform/src/index.ts
+++ packages/markdown-it-element-transform/src/index.ts
@@ -1,45 +1,37 @@
-import type { PluginSimple } from 'markdown-it'
+import type { PluginWithOptions } from 'markdown-it'
 import type Token from 'markdown-it/lib/token'
 
-/**
- * A markdown-it plugin to transform the token
- * @param options The options for the plugin
- * @param options.transformToken The function to transform the token
- * @returns The markdown-it plugin
- */
-const MarkdownItTokenTransform: (options: {
+export interface Options {
   /**
    * The function to transform the token
    * @param token The token to be transformed
    * @param env The environment of the markdown-it
    */
-  transformToken: (token: Token, env: any) => void
-}) => PluginSimple = (options) => {
-  return (md) => {
-    md.core.ruler.push(
-      'token_transform',
-      (state) => {
-        const transformFunc = options.transformToken === undefined ? function () { } : options.transformToken
-
-        state.tokens.forEach((token) => {
-          if (token.children && token.children.length) {
-            token.children.forEach((token) => {
-              transformFunc(token, state.env)
-            })
-          }
-          transformFunc(token, state.env)
-        })
-
-        return false
-      },
-    )
-  }
+  transform: (token: Token, env: any) => void
 }
 
 /**
  * A markdown-it plugin to transform the token
  * @param options The options for the plugin
- * @param options.transformToken The function to transform the token
+ * @param options.transform The function to transform the token
  * @returns The markdown-it plugin
  */
-export default MarkdownItTokenTransform
+export const ElementTransform: PluginWithOptions = (md, options: Options) => {
+  md.core.ruler.push(
+    'token_transform',
+    (state) => {
+      const transformFunc = options.transform === undefined ? function () { } : options.transform
+
+      state.tokens.forEach((token) => {
+        if (token.children && token.children.length) {
+          token.children.forEach((token) => {
+            transformFunc(token, state.env)
+          })
+        }
+        transformFunc(token, state.env)
+      })
+
+      return false
+    },
+  )
+}

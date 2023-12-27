0eba38ab7223ec9fef9afc8104ca20be9fc9aa1b
Neko Ayaka <neko@ayaka.moe>
2023-12-27 20:44:28 +0800

    styles: animation & transition
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-git-changelog/src/client/components/Changelog.vue
+++ packages/vitepress-plugin-git-changelog/src/client/components/Changelog.vue
@@ -2,7 +2,7 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/zh-cn'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 // @ts-expect-error virtual
 import ChangelogData from 'virtual:nolebase-git-changelog'
@@ -18,6 +18,8 @@ import { useI18n } from '../composables/i18n'
 const changelogData = ChangelogData as Changelog
 
 dayjs.extend(relativeTime)
+
+const toggleViewMore = ref(false)
 
 const rawPath = useRawPath()
 const commits = useCommits(changelogData.commits, rawPath)
@@ -36,56 +38,126 @@ const isFreshChange = computed(() => {
     return false
   return lastChangeDate.value.isAfter(dayjs().locale(lang.value.toLocaleLowerCase()).subtract(1, 'day'))
 })
+
+// enter-from
+function onBeforeEnter(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  divEl.style.transition = 'height 0.3s ease-in-out'
+  divEl.style.height = '0px'
+}
+
+function onEnter(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  divEl.style.height = `${divEl.scrollHeight}px`
+  divEl.style.overflow = 'hidden'
+}
+
+function onAfterEnter(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  divEl.style.height = `${divEl.scrollHeight}px`
+}
+
+function onBeforeLeave(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  divEl.style.height = `${divEl.scrollHeight}px`
+  divEl.style.overflow = 'hidden'
+}
+
+function onLeave(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  if (divEl.scrollHeight !== 0) {
+    divEl.style.transition = 'height 0.3s ease-in-out'
+    divEl.style.height = '0px'
+  }
+}
+
+function onAfterLeave(el: Element) {
+  const divEl = el as HTMLDivElement
+
+  divEl.style.transition = ''
+}
 </script>
 
 <template>
   <em v-if="!commits.length" opacity="70">{{ t('noLogs') }}</em>
-  <details v-else class="details custom-block [&_svg]:open:-rotate-180" :class="isFreshChange && '!bg-green/16'">
-    <summary style="list-style: none" class="flex select-none items-center justify-between hover:text-$vp-c-brand-1">
+  <div
+    v-else
+    :class="[
+      isFreshChange ? 'bg-green/16' : 'bg-$vp-custom-block-details-bg',
+    ]"
+    rounded-lg p-4
+  >
+    <div class="flex select-none items-center justify-between hover:text-$vp-c-brand-1" transition="color ease-in-out" duration-200>
       <span class="inline-flex items-center gap-3 text-$vp-custom-block-details-text">
         <span class="i-octicon:history-16" />
         <span v-if="commits[0]">
           {{ t('lastEdited', { props: { daysAgo: lastChangeDate?.fromNow() } }) }}
         </span>
       </span>
-      <span class="inline-flex items-center gap-3 !font-400">
-        <span>
-          {{ t('viewMore') }}
+      <label>
+        <input v-model="toggleViewMore" type="checkbox" invisible appearance-none>
+        <span class="inline-flex items-center gap-3 !font-400" cursor-pointer>
+          <span>
+            {{ t('viewMore') }}
+          </span>
+          <svg
+            class="i-octicon:chevron-down-16"
+            :class="[
+              toggleViewMore ? 'rotate-180' : 'rotate-0',
+            ]"
+            transition="transform ease-in-out"
+            duration-200
+          />
         </span>
-        <svg class="i-octicon:chevron-down-16" />
-      </span>
-    </summary>
-
-    <div class="grid grid-cols-[30px_auto] my-2 gap-1.5 children:my-auto -ml-1.5">
-      <template v-for="(commit) of commits" :key="commit.hash">
-        <template v-if="commit.tag">
-          <div m="t-1" />
-          <div m="t-1" />
-          <div class="m-auto h-7 w-7 inline-flex rounded-full bg-gray-400/10 text-sm opacity-90">
-            <div class="i-octicon:rocket-16" m="auto" />
-          </div>
-          <div>
-            <a :href="commit.release_tag_url" target="_blank">
-              <code class="!text-primary font-bold">{{ commit.tag }}</code>
-            </a>
-            <span class="text-xs opacity-50"> {{ t('committedOn') }} {{ new Date(commit.date).toLocaleDateString() }}</span>
-          </div>
-        </template>
-        <template v-else>
-          <div class="i-octicon:git-commit-16 m-auto rotate-90 transform opacity-30" />
-          <div>
-            <a :href="`${commit.repo_url}/commit/${commit.hash}`" target="_blank">
-              <code class="!hover:text-primary !text-xs !text-$vt-c-text-2">{{ commit.hash.slice(0, 5) }}</code>
-            </a>
-            <span text="sm">
-              -
-              <span v-html="renderCommitMessage(commit.repo_url || 'https://github.com/example/example', commit.message)" />
-            </span>
-
-            <span class="text-xs opacity-50">{{ t('committedOn', { props: { date: new Date(commit.date).toLocaleDateString() } }) }}</span>
-          </div>
-        </template>
-      </template>
+      </label>
     </div>
-  </details>
+    <Transition
+      @before-enter="onBeforeEnter"
+      @enter="onEnter"
+      @after-enter="onAfterEnter"
+      @before-leave="onBeforeLeave"
+      @leave="onLeave"
+      @after-leave="onAfterLeave"
+    >
+      <div
+        v-show="toggleViewMore"
+        class="grid grid-cols-[30px_auto] mt-2 gap-1.5 children:my-auto -ml-1.5"
+      >
+        <template v-for="(commit) of commits" :key="commit.hash">
+          <template v-if="commit.tag">
+            <div m="t-1" />
+            <div m="t-1" />
+            <div class="m-auto h-7 w-7 inline-flex rounded-full bg-gray-400/10 text-sm opacity-90">
+              <div class="i-octicon:rocket-16" m="auto" />
+            </div>
+            <div>
+              <a :href="commit.release_tag_url" target="_blank">
+                <code class="!text-primary font-bold">{{ commit.tag }}</code>
+              </a>
+              <span class="text-xs opacity-50"> {{ t('committedOn') }} {{ new Date(commit.date).toLocaleDateString() }}</span>
+            </div>
+          </template>
+          <template v-else>
+            <div class="i-octicon:git-commit-16 m-auto rotate-90 transform opacity-30" />
+            <div>
+              <a :href="`${commit.repo_url}/commit/${commit.hash}`" target="_blank">
+                <code class="!text-xs !text-$vp-c-brand-1 !hover:text-$vp-c-brand-1" transition="color ease-in-out" duration-200>{{ commit.hash.slice(0, 5) }}</code>
+              </a>
+              <span text="sm">
+                -
+                <span v-html="renderCommitMessage(commit.repo_url || 'https://github.com/example/example', commit.message)" />
+              </span>
+
+              <span class="text-xs opacity-50">{{ t('committedOn', { props: { date: new Date(commit.date).toLocaleDateString() } }) }}</span>
+            </div>
+          </template>
+        </template>
+      </div>
+    </Transition>
+  </div>
 </template>

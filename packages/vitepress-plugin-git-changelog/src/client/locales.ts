3f298f46c28862767ef3a04da75242e360fa3c9e
Neko Ayaka <neko@ayaka.moe>
2023-12-30 19:16:06 +0800

    feat(git-changelog): add view full history prompt text next to expand icon (#37)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-git-changelog/src/client/locales.ts
+++ packages/vitepress-plugin-git-changelog/src/client/locales.ts
@@ -3,11 +3,13 @@ import type { Locale } from './types'
 export const defaultEnLocale: Locale = {
   noLogs: 'No recent changes',
   lastEdited: 'Last edited {{daysAgo}}',
+  viewFullHistory: 'View full history',
   committedOn: ' on {{date}}',
 }
 
 export const defaultZhCNLocale: Locale = {
   noLogs: '暂无最近变更历史',
   lastEdited: '最后编辑于 {{daysAgo}}',
+  viewFullHistory: '查看完整历史',
   committedOn: ' 于 {{date}}',
 }

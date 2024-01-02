00b1d46108b48278d349a096328751ac67666a43
Neko Ayaka <neko@ayaka.moe>
2024-01-02 13:22:04 +0800

    docs: enable search, use more concurrent
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,7 +12,31 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/nolebase/integrations' },
     ],
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          'zh-CN': {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
+  buildConcurrency: 1000,
   locales: {
     'root': {
       label: 'English',

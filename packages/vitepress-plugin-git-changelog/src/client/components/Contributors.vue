a737f443489aa2fb83c0c9eb9fd40c22db1700da
Rizumu Ayaka <rizumu@ayaka.moe>
2024-01-13 11:01:39 +0800

    feat(git-changelog): multiple authors

--- packages/vitepress-plugin-git-changelog/src/client/components/Contributors.vue
+++ packages/vitepress-plugin-git-changelog/src/client/components/Contributors.vue
@@ -20,48 +20,66 @@ const options = inject(InjectionKey, {})
 const { t } = useI18n()
 const rawPath = useRawPath()
 const commits = useCommits(Changelog.commits, rawPath)
+const findRegisteredCreatorByName = (author_name: string) => options.mapContributors?.find(item => item.nameAliases && Array.isArray(item.nameAliases) && item.nameAliases.includes(author_name))
+const findRegisteredCreatorByEmail = (author_email: string) => options.mapContributors?.find(item => item.emailAliases && Array.isArray(item.emailAliases) && item.emailAliases.includes(author_email))
+
+const multipleAuthorsRegex = /^ *?Co-authored-by:(.*)(?:<|\(|\[|\{)(.*)(?:>|\)|\]|\}) *?$/mi
+function handleMultipleAuthors(map: Record<string, ContributorInfo>, c: Commit) {
+  let result: RegExpExecArray | null
+  multipleAuthorsRegex.lastIndex = 0
+  // eslint-disable-next-line no-cond-assign
+  while (result = multipleAuthorsRegex.exec(c.message)) {
+    const [, name, email] = result
+    handleCommitAuthors(map, name.trim(), email.trim(), '')
+  }
+}
+
+function handleCommitAuthors(map: Record<string, ContributorInfo>, authorName: string, authorEmail: string, authorAvatar: string) {
+  const targetCreatorByName = findRegisteredCreatorByName(authorName)
+  const targetCreatorByEmail = findRegisteredCreatorByEmail(authorEmail)
+
+  let name = ''
+  let avatar = ''
+  let url: string | undefined
+
+  if (targetCreatorByName) {
+    name = targetCreatorByName.name || authorName
+    avatar = targetCreatorByName.avatar || `https://gravatar.com/avatar/${authorAvatar}?d=retro`
+
+    const foundGitHubLink = targetCreatorByName.links?.find(item => item.type === 'github')
+    if (foundGitHubLink)
+      url = foundGitHubLink.link
+  }
+  else if (targetCreatorByEmail) {
+    name = targetCreatorByEmail.name || authorName
+    avatar = targetCreatorByEmail.avatar || `https://gravatar.com/avatar/${authorAvatar}?d=retro`
+
+    const foundGitHubLink = targetCreatorByEmail.links?.find(item => item.type === 'github')
+    if (foundGitHubLink)
+      url = foundGitHubLink.link
+  }
+  else {
+    name = authorName
+    avatar = `https://gravatar.com/avatar/${authorAvatar}?d=retro`
+  }
+
+  if (!map[name]) {
+    map[name] = {
+      name,
+      commitsCount: 0,
+      avatarUrl: avatar,
+      url,
+    }
+  }
+
+  map[name].commitsCount++
+}
 
 const contributors = computed<ContributorInfo[]>(() => {
   const map: Record<string, ContributorInfo> = {}
   commits.value.forEach((c) => {
-    const targetCreatorByName = options.mapContributors?.find(item => item.nameAliases && Array.isArray(item.nameAliases) && item.nameAliases.includes(c.author_name))
-    const targetCreatorByEmail = options.mapContributors?.find(item => item.emailAliases && Array.isArray(item.emailAliases) && item.emailAliases.includes(c.author_email))
-
-    let name = ''
-    let avatar = ''
-    let url: string | undefined
-
-    if (targetCreatorByName) {
-      name = targetCreatorByName.name || c.author_name
-      avatar = targetCreatorByName.avatar || `https://gravatar.com/avatar/${c.author_avatar}?d=retro`
-
-      const foundGitHubLink = targetCreatorByName.links?.find(item => item.type === 'github')
-      if (foundGitHubLink)
-        url = foundGitHubLink.link
-    }
-    else if (targetCreatorByEmail) {
-      name = targetCreatorByEmail.name || c.author_name
-      avatar = targetCreatorByEmail.avatar || `https://gravatar.com/avatar/${c.author_avatar}?d=retro`
-
-      const foundGitHubLink = targetCreatorByEmail.links?.find(item => item.type === 'github')
-      if (foundGitHubLink)
-        url = foundGitHubLink.link
-    }
-    else {
-      name = c.author_name
-      avatar = `https://gravatar.com/avatar/${c.author_avatar}?d=retro`
-    }
-
-    if (!map[name]) {
-      map[name] = {
-        name,
-        commitsCount: 0,
-        avatarUrl: avatar,
-        url,
-      }
-    }
-
-    map[name].commitsCount++
+    handleCommitAuthors(map, c.author_name, c.author_email, c.author_avatar)
+    handleMultipleAuthors(map, c)
   })
 
   return Object.values(map).sort((a, b) => b.commitsCount - a.commitsCount)

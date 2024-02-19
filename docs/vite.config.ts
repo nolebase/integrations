a42e9f6e8cfc2bc80b848213933d4e045207661c
Neko Ayaka <neko@ayaka.moe>
2024-02-19 18:09:29 +0700

    chore: update deps
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,58 +5,56 @@ import Inspect from 'vite-plugin-inspect'
 import { GitChangelog, GitChangelogMarkdownSection } from '@nolebase/vitepress-plugin-git-changelog/vite'
 import { PageProperties, PagePropertiesMarkdownSection } from '@nolebase/vitepress-plugin-page-properties/vite'
 
-export default defineConfig(async () => {
-  return {
-    assetsInclude: ['**/*.mov', '**/*.mp4'],
-    plugins: [
-      Inspect(),
-      UnoCSS(),
-      GitChangelog({
-        maxGitLogCount: 2000,
-        repoURL: () => 'https://github.com/nolebase/integrations',
-        rewritePaths: {
-          'docs/': '',
-        },
-      }),
-      GitChangelogMarkdownSection({
-        getChangelogTitle: (_, __, { helpers }): string => {
-          if (helpers.idStartsWith(join('pages', 'en')))
-            return 'File History'
-          if (helpers.idStartsWith(join('pages', 'zh-CN')))
-            return '文件历史'
-
+export default defineConfig({
+  assetsInclude: ['**/*.mov', '**/*.mp4'],
+  plugins: [
+    Inspect(),
+    UnoCSS(),
+    GitChangelog({
+      maxGitLogCount: 2000,
+      repoURL: () => 'https://github.com/nolebase/integrations',
+      rewritePaths: {
+        'docs/': '',
+      },
+    }),
+    GitChangelogMarkdownSection({
+      getChangelogTitle: (_, __, { helpers }): string => {
+        if (helpers.idStartsWith(join('pages', 'en')))
           return 'File History'
-        },
-        getContributorsTitle: (_, __, { helpers }): string => {
-          if (helpers.idStartsWith(join('pages', 'en')))
-            return 'Contributors'
-          if (helpers.idStartsWith(join('pages', 'zh-CN')))
-            return '贡献者'
+        if (helpers.idStartsWith(join('pages', 'zh-CN')))
+          return '文件历史'
 
+        return 'File History'
+      },
+      getContributorsTitle: (_, __, { helpers }): string => {
+        if (helpers.idStartsWith(join('pages', 'en')))
           return 'Contributors'
-        },
-        excludes: [],
-        exclude: (_, { helpers }): boolean => {
-          if (helpers.idEquals(join('pages', 'en', 'index.md')))
-            return true
-          if (helpers.idEquals(join('pages', 'zh-CN', 'index.md')))
-            return true
+        if (helpers.idStartsWith(join('pages', 'zh-CN')))
+          return '贡献者'
 
-          return false
-        },
-      }),
-      PageProperties(),
-      PagePropertiesMarkdownSection({
-        excludes: [],
-        exclude: (_, { helpers }): boolean => {
-          if (helpers.idEquals(join('pages', 'en', 'index.md')))
-            return true
-          if (helpers.idEquals(join('pages', 'zh-CN', 'index.md')))
-            return true
+        return 'Contributors'
+      },
+      excludes: [],
+      exclude: (_, { helpers }): boolean => {
+        if (helpers.idEquals(join('pages', 'en', 'index.md')))
+          return true
+        if (helpers.idEquals(join('pages', 'zh-CN', 'index.md')))
+          return true
 
-          return false
-        },
-      }),
-    ],
-  }
+        return false
+      },
+    }),
+    PageProperties(),
+    PagePropertiesMarkdownSection({
+      excludes: [],
+      exclude: (_, { helpers }): boolean => {
+        if (helpers.idEquals(join('pages', 'en', 'index.md')))
+          return true
+        if (helpers.idEquals(join('pages', 'zh-CN', 'index.md')))
+          return true
+
+        return false
+      },
+    }),
+  ],
 })

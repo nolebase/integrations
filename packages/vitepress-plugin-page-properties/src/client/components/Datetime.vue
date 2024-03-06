6c0796e065575ec3841451ab8680c22b6e3aa2ef
Neko Ayaka <neko@ayaka.moe>
2024-03-06 23:08:25 +0800

    chore(types): aligning vue version to prevent type mismatch (#90)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-page-properties/src/client/components/Datetime.vue
+++ packages/vitepress-plugin-page-properties/src/client/components/Datetime.vue
@@ -15,7 +15,7 @@ const { lang } = useData()
 <template>
   <span :title="String(props.value)">
     {{ props.pageProperty?.formatAsFrom
-      ? formatDistanceToNowFromValue(props.value, props.pageProperty?.dateFnsLocaleName || lang.value)
+      ? formatDistanceToNowFromValue(props.value, props.pageProperty?.dateFnsLocaleName || lang)
       : props.value
     }}
   </span>

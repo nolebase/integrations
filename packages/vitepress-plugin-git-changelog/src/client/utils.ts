16ab6616378340924c0381d71c987e4c32c9a43e
Neko Ayaka <neko@ayaka.moe>
2024-03-26 22:38:20 +0800

    fix(git-changelog): replaced deprecated md5 with uncrypto and fix the export issue
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-git-changelog/src/client/utils.ts
+++ packages/vitepress-plugin-git-changelog/src/client/utils.ts
@@ -1,6 +1,7 @@
 import { formatDistanceToNow, toDate } from 'date-fns'
 import type { Locale } from 'date-fns'
 import * as DateFnsLocales from 'date-fns/locale'
+import { subtle } from 'uncrypto'
 
 export function renderMarkdown(markdownText = '') {
   const htmlText = markdownText
@@ -35,4 +36,21 @@ export function formatDistanceToNowFromValue(value: Date, localeName = 'enUS') {
   catch (err) {
     return value
   }
+}
+
+/**
+ * Hashes a string using SHA-256
+ *
+ * Official example by MDN: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+ * @param {string} message - The message to be hashed
+ * @returns {Promise<string>} - The SHA-256 hash of the message
+ */
+export async function digestStringAsSHA256(message: string) {
+  const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
+  const hashBuffer = await subtle.digest('SHA-256', msgUint8) // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)) // convert buffer to byte array
+  const hashHex = hashArray
+    .map(b => b.toString(16).padStart(2, '0'))
+    .join('') // convert bytes to hex string
+  return hashHex
 }
--- packages/vitepress-plugin-git-changelog/src/client/virtual.d.ts
+++ packages/vitepress-plugin-git-changelog/src/client/virtual.d.ts
@@ -1,6 +1,21 @@
-declare module 'virtual:nolebase-git-changelog' {
-  import type { Commit } from '../types'
+interface Commit {
+  path: string[][]
+  tag?: string
+  release_tag_url?: string
+  hash: string
+  hash_url?: string
+  date: string
+  date_timestamp: number
+  message: string
+  refs?: string
+  body?: string
+  author_name: string
+  author_email: string
+  author_avatar: string
+  repo_url?: string
+}
 
+declare module 'virtual:nolebase-git-changelog' {
   interface Changelog {
     commits: Commit[]
   }

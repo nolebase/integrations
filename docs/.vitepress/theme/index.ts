7f48deab8fa5a2003f625fd1c498208cbe444dba
Neko Ayaka <neko@ayaka.moe>
2024-03-26 13:14:47 +0800

    refactor(unconfig-vitepress): supported plugins and plugin sets, implemented NolebasePluginSet (#120)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import { h } from 'vue'
 
-import { defineThemeConfig } from '@nolebase/unconfig-vitepress'
+import { NolebasePluginSet, defineThemeUnconfig } from '@nolebase/unconfig-vitepress'
 import { NuLazyDOMRiveCanvas } from '@nolebase/ui'
 
 import IntegrationCard from './components/IntegrationCard.vue'
@@ -9,14 +9,7 @@ import HomeContent from './components/HomeContent.vue'
 import './styles/vars.css'
 import './styles/main.css'
 
-export default defineThemeConfig<{
-  tags: string[]
-  authors: string[]
-  progress: string[]
-  createdAt: string
-  updatedAt: string
-  url1: string
-}>({
+export default defineThemeUnconfig({
   layout: {
     slots: {
       'layout-top': {
@@ -30,148 +23,32 @@ export default defineThemeConfig<{
     app.component('IntegrationCard', IntegrationCard)
     app.component('HomeContent', HomeContent)
   },
-  nolebase: {
-    enhancedReadabilities: {
-      enable: true,
-      options: {
-        layoutSwitch: {
-          defaultMode: 4,
-        },
-        spotlight: {
-          defaultToggle: true,
-        },
-      },
-    },
-    linkPreview: {
-      enable: true,
-    },
-    gitChangelog: {
-      enable: true,
-      options: {
-        mapContributors: [
-          {
-            name: 'Neko',
-            avatar: 'https://github.com/nekomeowww.png',
-            nameAliases: ['Neko Ayaka', 'Ayaka Neko'],
-            emailAliases: ['neko@ayaka.moe'],
-          },
-          {
-            name: 'Rizumu',
-            avatar: 'https://github.com/LittleSound.png',
-            nameAliases: ['Rizumu Ayaka', 'Ayaka Rizumu'],
-            emailAliases: ['rizumu@ayaka.moe'],
-          },
-          {
-            name: 'Nisekoi5',
-            avatar: 'https://github.com/Nisekoi5.png',
-            nameAliases: ['Nisekoi5'],
-          },
-        ],
-      },
-    },
-    pageProperties: {
-      enable: true,
-      options:
-      {
-        properties: {
-          'en': [
+  pluginSets: [
+    NolebasePluginSet({
+      gitChangelog: {
+        enable: true,
+        options: {
+          mapContributors: [
             {
-              key: 'tags',
-              type: 'tags',
-              title: 'Tags',
+              name: 'Neko',
+              avatar: 'https://github.com/nekomeowww.png',
+              nameAliases: ['Neko Ayaka', 'Ayaka Neko'],
+              emailAliases: ['neko@ayaka.moe'],
             },
             {
-              key: 'progress',
-              type: 'progress',
-              title: 'Progress',
+              name: 'Rizumu',
+              avatar: 'https://github.com/LittleSound.png',
+              nameAliases: ['Rizumu Ayaka', 'Ayaka Rizumu'],
+              emailAliases: ['rizumu@ayaka.moe'],
             },
             {
-              key: 'createdAt',
-              type: 'datetime',
-              title: 'Created at',
-              formatAsFrom: true,
-              dateFnsLocaleName: 'enUS',
-            },
-            {
-              key: 'updatedAt',
-              type: 'datetime',
-              title: 'Updated at',
-              formatAsFrom: true,
-              dateFnsLocaleName: 'enUS',
-            },
-            {
-              key: 'url1',
-              type: 'link',
-              title: 'URL 1',
-            },
-            {
-              key: 'wordsCount',
-              type: 'dynamic',
-              title: 'Word count',
-              options: {
-                type: 'wordsCount',
-              },
-            },
-            {
-              key: 'readingTime',
-              type: 'dynamic',
-              title: 'Reading time',
-              options: {
-                type: 'readingTime',
-                dateFnsLocaleName: 'enUS',
-              },
-            },
-          ],
-          'zh-CN': [
-            {
-              key: 'tags',
-              type: 'tags',
-              title: '标签',
-            },
-            {
-              key: 'progress',
-              type: 'progress',
-              title: '进度',
-            },
-            {
-              key: 'createdAt',
-              type: 'datetime',
-              title: '创建时间',
-              formatAsFrom: true,
-              dateFnsLocaleName: 'zhCN',
-            },
-            {
-              key: 'updatedAt',
-              type: 'datetime',
-              title: '更新时间',
-              formatAsFrom: true,
-              dateFnsLocaleName: 'zhCN',
-            },
-            {
-              key: 'url1',
-              type: 'link',
-              title: '链接 1',
-            },
-            {
-              key: 'wordsCount',
-              type: 'dynamic',
-              title: '字数',
-              options: {
-                type: 'wordsCount',
-              },
-            },
-            {
-              key: 'readingTime',
-              type: 'dynamic',
-              title: '阅读时间',
-              options: {
-                type: 'readingTime',
-                dateFnsLocaleName: 'zhCN',
-              },
+              name: 'Nisekoi5',
+              avatar: 'https://github.com/Nisekoi5.png',
+              nameAliases: ['Nisekoi5'],
             },
           ],
         },
       },
-    },
-  },
+    }),
+  ],
 })
---
+++ packages/unconfig-vitepress/src/plugins/nolebase/index.ts
@@ -0,0 +1,241 @@
+import { h } from 'vue'
+
+import {
+  LayoutMode,
+  type Options as NolebaseEnhancedReadabilitiesOptions,
+} from '@nolebase/vitepress-plugin-enhanced-readabilities'
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesPlugin,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from '@nolebase/vitepress-plugin-enhanced-readabilities'
+
+import type {
+  Options as NolebaseInlineLinkPreviewOptions,
+} from '@nolebase/vitepress-plugin-inline-link-preview'
+import {
+  NolebaseInlineLinkPreviewPlugin,
+} from '@nolebase/vitepress-plugin-inline-link-preview'
+
+import {
+  NolebaseHighlightTargetedHeading,
+  NolebaseNolebaseHighlightTargetedHeadingPlugin,
+} from '@nolebase/vitepress-plugin-highlight-targeted-heading'
+
+import type {
+  Options as NolebaseGitChangelogOptions,
+} from '@nolebase/vitepress-plugin-git-changelog/client'
+import {
+  NolebaseGitChangelogPlugin,
+} from '@nolebase/vitepress-plugin-git-changelog/client'
+
+import type {
+  Options as NolebasePagePropertiesOptions,
+} from '@nolebase/vitepress-plugin-page-properties/client'
+import {
+  NolebasePagePropertiesPlugin,
+} from '@nolebase/vitepress-plugin-page-properties/client'
+
+import type { PluginSet } from '../../types'
+
+export interface NolebasePluginSetOptions<PagePropertiesObject extends object = any> {
+  enhancedReadabilities?: {
+    enable?: boolean
+    options?: NolebaseEnhancedReadabilitiesOptions
+  }
+  highlightTargetedHeading?: {
+    enable?: boolean
+  }
+  linkPreview?: {
+    enable?: boolean
+    options?: NolebaseInlineLinkPreviewOptions
+  }
+  gitChangelog?: {
+    enable?: boolean
+    options?: NolebaseGitChangelogOptions
+  }
+  pageProperties?: {
+    enable?: boolean
+    options?: NolebasePagePropertiesOptions<PagePropertiesObject>
+  }
+}
+
+const defaultOptions: NolebasePluginSetOptions = {
+  enhancedReadabilities: {
+    enable: true,
+    options: {
+      layoutSwitch: {
+        defaultMode: LayoutMode.BothWidthAdjustable,
+      },
+      spotlight: {
+        defaultToggle: true,
+      },
+    },
+  },
+  highlightTargetedHeading: {
+    enable: true,
+  },
+  linkPreview: {
+    enable: true,
+  },
+  gitChangelog: {
+    enable: true,
+    options: {
+      mapContributors: [],
+    },
+  },
+  pageProperties: {
+    enable: true,
+    options:
+    {
+      properties: {
+        'en': [
+          {
+            key: 'tags',
+            type: 'tags',
+            title: 'Tags',
+          },
+          {
+            key: 'progress',
+            type: 'progress',
+            title: 'Progress',
+          },
+          {
+            key: 'createdAt',
+            type: 'datetime',
+            title: 'Created at',
+            formatAsFrom: true,
+            dateFnsLocaleName: 'enUS',
+          },
+          {
+            key: 'updatedAt',
+            type: 'datetime',
+            title: 'Updated at',
+            formatAsFrom: true,
+            dateFnsLocaleName: 'enUS',
+          },
+          {
+            key: 'wordsCount',
+            type: 'dynamic',
+            title: 'Word count',
+            options: {
+              type: 'wordsCount',
+            },
+          },
+          {
+            key: 'readingTime',
+            type: 'dynamic',
+            title: 'Reading time',
+            options: {
+              type: 'readingTime',
+              dateFnsLocaleName: 'enUS',
+            },
+          },
+        ],
+        'zh-CN': [
+          {
+            key: 'tags',
+            type: 'tags',
+            title: '标签',
+          },
+          {
+            key: 'progress',
+            type: 'progress',
+            title: '进度',
+          },
+          {
+            key: 'createdAt',
+            type: 'datetime',
+            title: '创建时间',
+            formatAsFrom: true,
+            dateFnsLocaleName: 'zhCN',
+          },
+          {
+            key: 'updatedAt',
+            type: 'datetime',
+            title: '更新时间',
+            formatAsFrom: true,
+            dateFnsLocaleName: 'zhCN',
+          },
+          {
+            key: 'wordsCount',
+            type: 'dynamic',
+            title: '字数',
+            options: {
+              type: 'wordsCount',
+            },
+          },
+          {
+            key: 'readingTime',
+            type: 'dynamic',
+            title: '阅读时间',
+            options: {
+              type: 'readingTime',
+              dateFnsLocaleName: 'zhCN',
+            },
+          },
+        ],
+      },
+    },
+  },
+}
+
+function applyOptionsWithDefaults<PagePropertiesObject extends object = any>(options: NolebasePluginSetOptions<any>): NolebasePluginSetOptions<PagePropertiesObject> {
+  const mergedOptions: NolebasePluginSetOptions<any> = { ...options }
+  for (const key in defaultOptions) {
+    const k = key as keyof NolebasePluginSetOptions<any>
+
+    if (typeof mergedOptions[k] === 'undefined' || !mergedOptions[k])
+      mergedOptions[k] = defaultOptions[k]
+  }
+
+  return mergedOptions as NolebasePluginSetOptions<PagePropertiesObject>
+}
+
+export function NolebasePluginSet<PagePropertiesObject extends object = any>(
+  options?: NolebasePluginSetOptions<PagePropertiesObject>,
+): PluginSet {
+  const opts = applyOptionsWithDefaults(options || {})
+
+  return {
+    configureLayout({ helpers }) {
+      if (opts.highlightTargetedHeading?.enable)
+        helpers.defineSlot('layout-top', () => h(NolebaseHighlightTargetedHeading))
+
+      if (opts.enhancedReadabilities?.enable) {
+        helpers.defineSlot('nav-bar-content-after', () => h(NolebaseEnhancedReadabilitiesMenu))
+        helpers.defineSlot('nav-screen-content-after', () => h(NolebaseEnhancedReadabilitiesScreenMenu))
+      }
+    },
+    async enhanceApp({ app }) {
+      if (opts.enhancedReadabilities?.enable) {
+        const enhancedReadabilitiesOptions = opts.enhancedReadabilities?.options ? [opts.enhancedReadabilities.options] : []
+        app.use(NolebaseEnhancedReadabilitiesPlugin, ...enhancedReadabilitiesOptions)
+
+        await import('@nolebase/vitepress-plugin-enhanced-readabilities/dist/style.css')
+      }
+
+      if (opts.highlightTargetedHeading?.enable)
+        app.use(NolebaseNolebaseHighlightTargetedHeadingPlugin)
+
+      if (opts.linkPreview?.enable) {
+        const linkPreviewOptions = opts.linkPreview?.options ? [opts.linkPreview.options] : []
+        app.use(NolebaseInlineLinkPreviewPlugin, ...linkPreviewOptions)
+
+        await import('@nolebase/vitepress-plugin-inline-link-preview/dist/style.css')
+      }
+
+      if (opts.gitChangelog?.enable) {
+        const gitChangelogOptions = opts.gitChangelog?.options ? [opts.gitChangelog.options] : []
+        app.use(NolebaseGitChangelogPlugin, ...gitChangelogOptions)
+      }
+
+      if (opts.pageProperties?.enable) {
+        const pagePropertiesOptions = opts.pageProperties?.options ? [opts.pageProperties.options] : []
+        app.use(NolebasePagePropertiesPlugin<PagePropertiesObject>(), ...pagePropertiesOptions)
+
+        await import('@nolebase/vitepress-plugin-page-properties/client/style.css')
+      }
+    },
+  }
+}

8569935b564e509843649bd7d4442da0904237c4
Neko Ayaka <neko@ayaka.moe>
2024-03-27 19:49:44 +0800

    fix(ui|git-changelog): missing configurations for @rive-app/canvas (#131)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,14 +2,17 @@ import type { App } from 'vue'
 
 import NuButton from './components/NuButton.vue'
 import NuVerticalTransition from './components/NuVerticalTransition.vue'
+import NuLazyTeleportRiveCanvas from './components/NuLazyTeleportRiveCanvas.vue'
 
 export {
   NuButton,
   NuVerticalTransition,
+  NuLazyTeleportRiveCanvas,
 }
 
 export function install(app: App): void {
   app.component('NuButton', NuButton)
   // Animations
+  app.component('NuLazyTeleportRiveCanvas', NuLazyTeleportRiveCanvas)
   app.component('NuVerticalTransition', NuVerticalTransition)
 }

9cd90afed556f9e7ed683787dc32de10e583d26f
Neko Ayaka <neko@ayaka.moe>
2024-04-05 20:35:16 +0800

    fix(element-transform): incorrect type for options (#147)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/markdown-it-element-transform/src/index.ts
+++ packages/markdown-it-element-transform/src/index.ts
@@ -1,13 +1,15 @@
 import type { PluginWithOptions } from 'markdown-it'
+import type StateCore from 'markdown-it/lib/rules_core/state_core'
 import type Token from 'markdown-it/lib/token'
 
 export interface Options {
   /**
    * The function to transform the token
-   * @param token The token to be transformed
-   * @param env The environment of the markdown-it
+   * @param {Token} token The token to be transformed
+   * @param {StateCore} state The state of the markdown-it
+   * @param {object} env The environment of the markdown-it
    */
-  transform: (token: Token, env: any) => void
+  transform: (token: Token, state: StateCore, env: any) => void
 }
 
 /**
@@ -16,7 +18,10 @@ export interface Options {
  * @param options The options for the plugin
  * @param options.transform The function to transform the token
  */
-export const ElementTransform: PluginWithOptions = (md, options: Options) => {
+export const ElementTransform: PluginWithOptions<Options> = (md, options) => {
+  if (!options || !options.transform)
+    throw new Error('The `transform` option is required')
+
   md.core.ruler.push(
     'token_transform',
     (state) => {
@@ -25,10 +30,11 @@ export const ElementTransform: PluginWithOptions = (md, options: Options) => {
       state.tokens.forEach((token) => {
         if (token.children && token.children.length) {
           token.children.forEach((token) => {
-            transformFunc(token, state.env)
+            transformFunc(token, state, state.env)
           })
         }
-        transformFunc(token, state.env)
+
+        transformFunc(token, state, state.env)
       })
 
       return false

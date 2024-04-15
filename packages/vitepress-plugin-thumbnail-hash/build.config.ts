9751870d4338566fa091394a5a8a4d1e62366a3f
Neko Ayaka <neko@ayaka.moe>
2024-04-15 12:09:52 +0800

    feat(thumbnail-hash): build extra styles
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-thumbnail-hash/build.config.ts
+++ packages/vitepress-plugin-thumbnail-hash/build.config.ts
@@ -1,5 +1,10 @@
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
+
 import { defineBuildConfig } from 'unbuild'
 import builtins from 'builtin-modules'
+
+const execAsync = promisify(exec)
 
 export default defineBuildConfig({
   entries: [
@@ -24,5 +29,20 @@ export default defineBuildConfig({
   ],
   rollup: {
     emitCJS: true,
+  },
+  hooks: {
+    'mkdist:done': async (ctx) => {
+      if (ctx.options.stub)
+        return
+      // Since not all the users would choose to use unocss,
+      // we bundle the styles from unocss here for users to opt-in.
+      //
+      // However, since the unocss doesn't provide a unplugin, or rollup
+      // plugin for us to use, we have to use the CLI here.
+      //
+      // The use of CLI was suggested by how to use unocss with rollup? · unocss/unocss · Discussion #542
+      // https:// github.com/unocss/unocss/discussions/542
+      await execAsync('unocss "./src/client/**/*.vue" -o dist/client/style.css')
+    },
   },
 })

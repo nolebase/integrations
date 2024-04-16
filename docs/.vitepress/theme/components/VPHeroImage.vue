edf351bd07e0a1c6ba7db223a72430b3ec7a55ec
Neko Ayaka <neko@ayaka.moe>
2024-04-17 00:11:30 +0800

    docs: tweaked color
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>
    Co-authored-by: Rizumu Ayaka <rizumu@ayaka.moe>

--- docs/.vitepress/theme/components/VPHeroImage.vue
+++ docs/.vitepress/theme/components/VPHeroImage.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useData } from 'vitepress'
+// import { computed } from 'vue'
+// import { useData } from 'vitepress'
 
-const { isDark } = useData()
-const src = computed(() => isDark.value ? '/logo-dark.png' : '/logo-day.png')
+// const { isDark } = useData()
+// const src = computed(() => isDark.value ? '/logo-dark.png' : '/logo-day.png')
 </script>
 
 <template>
   <img
-    :src="src"
-    class="VPImage image-src"
+    src="/logo-light.png"
+    class="VPImage image-src block dark:hidden"
+    alt="Hero Image"
+  >
+  <img
+    src="/logo-dark.png"
+    class="VPImage image-src hidden dark:block"
     alt="Hero Image"
   >
 </template>

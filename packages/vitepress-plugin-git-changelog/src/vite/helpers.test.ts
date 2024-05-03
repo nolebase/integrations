9da0e59f50196678c7c8f25229ed84518ad641ed
Northword <northword@outlook.com>
2024-05-03 17:24:47 +0800

    test: use example author info

--- packages/vitepress-plugin-git-changelog/src/vite/helpers.test.ts
+++ packages/vitepress-plugin-git-changelog/src/vite/helpers.test.ts
@@ -5,7 +5,6 @@ import {
   defaultCommitURLHandler,
   defaultReleaseTagURLHandler,
   defaultReleaseTagsURLHandler,
-  generateCommitPathsRegExp,
   parseCommits,
   parseGitLogRefsAsTags,
   rewritePathsByPatterns,
@@ -101,7 +100,7 @@ describe('parseGitLogRefsAsTags', () => {
 
 describe('parseCommits', () => {
   it('should init commit with fields transformed', async () => {
-    const mockedCommits = ['c16db1033fce57f50b261e9944c136a26fcaccc6|Neko Ayaka|neko@ayaka.moe|Mon Mar 25 20:05:53 2024 +0800|release: v1.24.3| (tag: v1.24.3)|Signed-off-by: Neko Ayaka <neko@ayaka.moe>\n']
+    const mockedCommits = ['c16db1033fce57f50b261e9944c136a26fcaccc6|First Last|user@example.com|Mon Mar 25 20:05:53 2024 +0800|release: v1.24.3| (tag: v1.24.3)|Signed-off-by: First Last <user@example.com>\n']
 
     const commit = await parseCommits(
       '',
@@ -123,16 +122,16 @@ describe('parseCommits', () => {
       date: 'Mon Mar 25 20:05:53 2024 +0800',
       date_timestamp: 1711368353000,
       message: 'release: v1.24.3',
-      body: 'Signed-off-by: Neko Ayaka <neko@ayaka.moe>\n',
-      author_name: 'Neko Ayaka',
-      author_email: 'neko@ayaka.moe',
-      author_avatar: '8002ecc992a1c4aba4c49fd04ea4b48ecf2468b85d3b9cdd6dae4163bf1bb945',
+      body: 'Signed-off-by: First Last <user@example.com>\n',
+      author_name: 'First Last',
+      author_email: 'user@example.com',
+      author_avatar: 'b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514',
       repo_url: 'https://github.com/example-org/example',
     }])
   })
 
   it('should transform for commit contains no refs', async () => {
-    const mockedCommit = ['62ef7ed8f54ea1faeacf6f6c574df491814ec1b1|Neko Ayaka|neko@ayaka.moe|Wed Apr 24 14:24:44 2024 +0800|docs: fix english integrations list||Signed-off-by: Neko Ayaka <neko@ayaka.moe>\n']
+    const mockedCommit = ['62ef7ed8f54ea1faeacf6f6c574df491814ec1b1|First Last|user@example.com|Wed Apr 24 14:24:44 2024 +0800|docs: fix english integrations list||Signed-off-by: First Last <user@example.com>\n']
     const commit = await parseCommits(
       '',
       mockedCommit,
@@ -149,16 +148,16 @@ describe('parseCommits', () => {
       date: 'Wed Apr 24 14:24:44 2024 +0800',
       date_timestamp: 1713939884000,
       message: 'docs: fix english integrations list',
-      body: 'Signed-off-by: Neko Ayaka <neko@ayaka.moe>\n',
-      author_name: 'Neko Ayaka',
-      author_email: 'neko@ayaka.moe',
-      author_avatar: '8002ecc992a1c4aba4c49fd04ea4b48ecf2468b85d3b9cdd6dae4163bf1bb945',
+      body: 'Signed-off-by: First Last <user@example.com>\n',
+      author_name: 'First Last',
+      author_email: 'user@example.com',
+      author_avatar: 'b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514',
       repo_url: 'https://github.com/example-org/example',
     }])
   })
 
   it('should transform for commit contains no body', async () => {
-    const mockedCommit = ['fa0fb328b988499c74983e9164f6db4e2e92afd8|Rizumu Ayaka|rizumu@ayaka.moe|Sun Apr 7 22:27:57 2024 +0800|release: v1.28.0| (tag: v1.28.0)|']
+    const mockedCommit = ['fa0fb328b988499c74983e9164f6db4e2e92afd8|First Last|user@example.com|Sun Apr 7 22:27:57 2024 +0800|release: v1.28.0| (tag: v1.28.0)|']
     const commit = await parseCommits(
       '',
       mockedCommit,
@@ -175,9 +174,9 @@ describe('parseCommits', () => {
       date_timestamp: 1712500077000,
       message: 'release: v1.28.0',
       body: '',
-      author_name: 'Rizumu Ayaka',
-      author_email: 'rizumu@ayaka.moe',
-      author_avatar: '898fff9a9daaaad0a2bf6e1ae59a3610189b9ec42a7bc6c26c7e9f855cf8576e',
+      author_name: 'First Last',
+      author_email: 'user@example.com',
+      author_avatar: 'b4c9a289323b21a01c3e940f150eb9b8c542587f1abfd8f0e1cc1ffc5e475514',
       repo_url: 'https://github.com/example-org/example',
       hash_url: 'https://github.com/example-org/example/commit/fa0fb328b988499c74983e9164f6db4e2e92afd8',
       tags: ['v1.28.0'],

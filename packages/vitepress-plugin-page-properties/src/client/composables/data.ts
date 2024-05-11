a821f752b723edc4776788576ab65d56b98a5ffe
Neko Ayaka <neko@ayaka.moe>
2024-05-11 16:18:34 +0800

    fix(page-properties): incorrect implementation of hmr
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- packages/vitepress-plugin-page-properties/src/client/composables/data.ts
+++ packages/vitepress-plugin-page-properties/src/client/composables/data.ts
@@ -1,23 +1,99 @@
-import { reactive } from 'vue'
+import { type ComputedRef, type Ref, computed, ref, toValue } from 'vue'
 import VirtualPagePropertiesData from 'virtual:nolebase-page-properties'
+import type { PageData } from 'vitepress'
 
 type Data = typeof VirtualPagePropertiesData
-const pagePropertiesData = reactive<Data>(VirtualPagePropertiesData)
 
-export function usePageProperties() {
-  if (Object.keys(pagePropertiesData).length === 0
-    && Object.keys(VirtualPagePropertiesData).length !== 0) {
-    Object.keys(VirtualPagePropertiesData).forEach((key) => {
-      pagePropertiesData[key] = VirtualPagePropertiesData[key]
-    })
+type AggregatedPageProperties = Array<{
+  key: PropertyKey | 'unknown'
+  pageProperty: Property<any>
+  value: any
+  omitEmpty: boolean
+}>
+
+function normalizeProperties(
+  frontmatter: Record<string, any>,
+  propertyConfig: Property<any>,
+  calculatedPropertiesActualData: Data[string],
+): AggregatedPageProperties[number] {
+  const { key } = propertyConfig
+
+  const baseResolvedProperties: AggregatedPageProperties[number] = {
+    key: 'unknown',
+    pageProperty: propertyConfig,
+    value: '',
+    omitEmpty: true,
   }
 
+  if (propertyConfig.type === 'dynamic') {
+    if (propertyConfig.key)
+      baseResolvedProperties.key = propertyConfig.key
+    else
+      baseResolvedProperties.key = propertyConfig.type
+
+    baseResolvedProperties.value = calculatedPropertiesActualData || ''
+    baseResolvedProperties.omitEmpty = false
+
+    return baseResolvedProperties
+  }
+
+  if (!key)
+    return baseResolvedProperties
+
+  baseResolvedProperties.key = propertyConfig.key
+
+  if ('omitEmpty' in propertyConfig)
+    baseResolvedProperties.omitEmpty = !!propertyConfig.omitEmpty
+
+  if (!Object.keys(frontmatter).length || !frontmatter[String(key)])
+    return baseResolvedProperties
+
+  baseResolvedProperties.value = frontmatter[String(key)]
+
+  return baseResolvedProperties
+}
+
+export function usePageProperties(pageData: Ref<PageData>, userConfiguredPageProperties: ComputedRef<Property<any>[]>) {
+  const pagePropertiesData = ref<Data>({ ...VirtualPagePropertiesData })
+  if (!pagePropertiesData.value)
+    pagePropertiesData.value = { }
+
   return {
-    data: pagePropertiesData,
-    applyPagePropertiesData(data: typeof VirtualPagePropertiesData) {
-      Object.keys(data).forEach((key) => {
-        pagePropertiesData[key] = data[key]
+    config: computed<AggregatedPageProperties>(() => {
+      if (!userConfiguredPageProperties.value || !userConfiguredPageProperties.value.length)
+        return []
+
+      const currentPath = toValue(pageData.value.filePath)
+      const matchedPagePropertiesForCurrentPath = pagePropertiesData.value[currentPath]
+      if (!matchedPagePropertiesForCurrentPath || !Object.keys(matchedPagePropertiesForCurrentPath).length)
+        return []
+
+      const mPageProperties = userConfiguredPageProperties.value.map(item => normalizeProperties(
+        pageData.value.frontmatter,
+        item,
+        matchedPagePropertiesForCurrentPath,
+      ))
+
+      return mPageProperties.filter((item) => {
+        if (item.omitEmpty && !item.value)
+          return false
+
+        return true
       })
+    }),
+    data: computed<Data[string]>(() => {
+      const currentPath = toValue(pageData.value.filePath)
+      const matchedPagePropertiesForCurrentPath = pagePropertiesData.value[currentPath]
+      if (!matchedPagePropertiesForCurrentPath || !Object.keys(matchedPagePropertiesForCurrentPath).length)
+        return { readingTime: 0, wordsCount: 0 }
+
+      return {
+        readingTime: matchedPagePropertiesForCurrentPath.readingTime || 0,
+        wordsCount: matchedPagePropertiesForCurrentPath.wordsCount || 0,
+      }
+    }),
+    update(data: typeof VirtualPagePropertiesData) {
+      pagePropertiesData.value = { ...data }
     },
   }
 }

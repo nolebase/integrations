9a378cc7b554195dfa07d2e4a4bbb7fbff4a8bc8
Jimbo <happycoder@foxmail.com>
2024-07-30 16:50:03 +0800

    ## 代码风格与工具配置更新
    ### 工作区配置更新
    - 修改了`integrations.code-workspace`文件中的`typescript.tsdk`设置项，去除了重复的路径部分。
    - 更新`pnpm-workspace.yaml`文件中的包路径匹配规则，将通配符`**`替换为`*`，并调整了排除目录的规则。
    - 调整了`tsconfig.json`中的模块解析配置，优化了路径别名的定义，并添加了通配符路径映射。
    
    ### 代码依赖更新
    - 更新了`asciinema-player`和`@rive-app/canvas`的版本，以解决相关依赖问题。
    - 在`pnpm-lock.yaml`文件中，对多个依赖包版本进行了更新和依赖项的整理，确保项目依赖的最新性和稳定性。
    
    ### 代码质量优化
    - 在`vitepress-plugin-enhanced-readabilities`包中，将`VPFlyout`组件的导入方式改为异步组件加载，优化了代码的加载性能。
    
    ### 问题修复
    - 修复了`seroval-plugins`和`seroval`包的相关依赖问题，提高了项目的整体稳定性和兼容性。
    
    通过这些改动，项目的工作区配置得到了优化，代码依赖和版本得到了更新，同时通过代码质量和性能的优化，提高了项目的整体可维护性和用户体验。

--- packages/vitepress-plugin-enhanced-readabilities/src/client/components/deps.ts
+++ packages/vitepress-plugin-enhanced-readabilities/src/client/components/deps.ts
@@ -1,3 +1,5 @@
-import _VPFlyout from 'vitepress/dist/client/theme-default/components/VPFlyout.vue'
+import { defineAsyncComponent } from 'vue'
+
+const _VPFlyout = defineAsyncComponent(() => import('vitepress/dist/client/theme-default/components/VPFlyout.vue'));
 // Isolate the types of Vitepress internal components to avoid typecheck errors
 export const VPFlyout: any = _VPFlyout

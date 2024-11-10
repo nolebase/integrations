d5628e9e496fdd7e67852f603cb560765580bbe2
LemonNeko <chheese048@gmail.com>
2024-11-10 16:45:30 +0800

    use rollup instead of mkdist

--- packages/vitepress-plugin-breadcrumbs/build.config.ts
+++ packages/vitepress-plugin-breadcrumbs/build.config.ts
@@ -5,9 +5,11 @@ export default defineBuildConfig({
     { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.vue'], loaders: ['vue'] },
     { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.ts'], format: 'cjs', loaders: ['js'] },
     { builder: 'mkdist', input: './src/client', outDir: './dist/client', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
-    { builder: 'mkdist', input: './src/vitepress', outDir: './dist/vitepress', pattern: ['**/*.ts'], format: 'cjs', loaders: ['js'] },
-    { builder: 'mkdist', input: './src/vitepress', outDir: './dist/vitepress', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
+    { builder: 'rollup', input: './src/vitepress/index', outDir: 'dist/vitepress/' },
   ],
+  rollup: {
+    emitCJS: true,
+  },
   clean: true,
   sourcemap: true,
   declaration: true,

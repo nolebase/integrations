2d6cecf4597da2d56e5aa04299c44554829e568c
Dustella <fdnoaivj@outlook.com>
2024-11-29 15:23:22 +0800

    style: breadcrumbs now warps line correctly

--- packages/vitepress-plugin-breadcrumbs/src/client/components/NolebaseBreadcrumbs.vue
+++ packages/vitepress-plugin-breadcrumbs/src/client/components/NolebaseBreadcrumbs.vue
@@ -2,45 +2,48 @@
 import { useData } from 'vitepress'
 
 const { frontmatter } = useData()
+
+const breadcrumbs = (frontmatter.value.breadcrumbs ?? []) as {
+  title: string
+  link: string
+}[]
 </script>
 
 <template>
-  <div class="vp-nolebase-breadcrumbs">
-    <span v-for="item in frontmatter.breadcrumbs" :key="item">
-      <a v-if="item.link" :href="item.link">{{ item.title }}</a>
-      <span v-else>{{ item.title }}</span>
+  <div class="breadcrumbs">
+    <span v-for="item in breadcrumbs" :key="item">
+      <a :href="item.link">{{ item.title }}</a>
     </span>
   </div>
 </template>
 
 <style scoped>
-.vp-nolebase-breadcrumbs {
-  display: flex;
-  gap: 8px;
+.breadcrumbs {
   font-size: 14px;
   margin-bottom: 2rem;
 }
 
-.vp-nolebase-breadcrumbs span {
+.breadcrumbs span {
   transition: color 0.25s, opacity 0.25s;
   color: var(--vp-c-text-2);
 }
 
-.vp-nolebase-breadcrumbs span:hover {
+.breadcrumbs span:hover {
   color: var(--vp-c-brand-1);
 }
 
-.vp-nolebase-breadcrumbs span:last-child {
+.breadcrumbs span:last-child {
   color: var(--vp-c-text-1);
 }
 
-.vp-nolebase-breadcrumbs span:last-child:hover {
+.breadcrumbs span:last-child:hover {
   color: var(--vp-c-brand-1);
 }
 
-.vp-nolebase-breadcrumbs span:not(:first-child)::before {
-  content: '/';
-  padding-right: 8px;
+.breadcrumbs span:not(:first-child)::before {
+  content: "/";
+  padding-left: 4px;
+  padding-right: 4px;
   color: var(--vp-c-text-3);
 }
 </style>

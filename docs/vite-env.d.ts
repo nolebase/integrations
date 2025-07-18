8bdf15dd271cf1434a3647bab613fe616626c3f7
Northword <44738481+northword@users.noreply.github.com>
2025-07-18 20:51:28 +0800

    chore: use pnpm catalog (#524)

--- docs/vite-env.d.ts
+++ docs/vite-env.d.ts
@@ -1,6 +1,10 @@
 // Env Variables and Modes | Vite
 // https://vitejs.dev/guide/env-and-mode#intellisense-for-typescript
 
+/// <reference types="vite/client" />
+/// <reference types="@nolebase/vitepress-plugin-git-changelog/types/virtual.d.ts" />
+/// <reference types="@nolebase/vitepress-plugin-page-properties/client/virtual.d.ts" />
+
 interface ImportMetaEnv {
   readonly VUE_PROD_HYDRATION_MISMATCH_DETAILS_FLAG?: '1'
 }

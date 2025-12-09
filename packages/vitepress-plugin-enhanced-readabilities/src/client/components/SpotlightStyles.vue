099fa496883ecc4669690aaf7217e64c62dcf016
shacha <2807665861@qq.com>
2025-12-09 13:46:27 +0800

    fix: #572

--- packages/vitepress-plugin-enhanced-readabilities/src/client/components/SpotlightStyles.vue
+++ packages/vitepress-plugin-enhanced-readabilities/src/client/components/SpotlightStyles.vue
@@ -117,6 +117,7 @@ watch(isTouchScreen, () => {
 .fade-shift-enter-active,
 .fade-shift-leave-active {
   transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out, max-height 0.2s ease-in-out;
+  overflow: hidden;
 }
 
 .fade-shift-enter-from,
@@ -124,5 +125,6 @@ watch(isTouchScreen, () => {
   opacity: 0;
   transform: translateY(-8px);
   max-height: 0;
+  overflow: hidden;
 }
 </style>

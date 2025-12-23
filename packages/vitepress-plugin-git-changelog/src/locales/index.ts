8514ab54ea0cda3f779c6d18d7521fe981ee1850
Cherry🍒 <132185099+dongguacute@users.noreply.github.com>
2025-12-23 13:48:50 +0800

    feat(locales): add traditional chinese translations for git changelog (#580)

--- packages/vitepress-plugin-git-changelog/src/locales/index.ts
+++ packages/vitepress-plugin-git-changelog/src/locales/index.ts
@@ -1,9 +1,11 @@
 import defaultEnLocale from './en.yaml'
 import defaultRuLocale from './ru-RU.yaml'
 import defaultZhCNLocale from './zh-CN.yaml'
+import defaultZhHantLocale from './zh-Hant.yaml'
 
 export {
   defaultEnLocale,
   defaultRuLocale,
   defaultZhCNLocale,
+  defaultZhHantLocale,
 }
